13c8b3ec59e8b2d33cd5faafccdda0fa5e34b07a
jesiel364 <82860253+jesiel364@users.noreply.github.com>
2022-04-06 13:02:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from kivy.storage.jsonstore import JsonStore
 
 store = JsonStore('hello.json')
 
-# put alguns valores
-#store.put('tito', name='Mathieu', age=30)
+# PUT
+store.put('tito', name='Mathieu', age=30)
 #store.put('tshirtman', name='Gabriel', age=27)
 #store.put('Ziel', name='Jesiel', age=21)
 
-# pegando por uma key
-print('tito is', store.get('tito'))
+# GET
+print('tito tem ', store.get('tito')['age'])
+
+# DELETE
+#store.delete('tito')
 
 # or guess the key/entry for a part of the key
 #key, tshirtman = store.find(name='Gabriel')
 #print('tshirtman is', tshirtman)
 
+# FIND
 # Filtrando items
 res = store.find()
 for item in res:
-    #print("Nome=", str(item))
     print('Nome: ', str(item[1]['name']))
     print("Idade: ", str(item[1]['age']))
